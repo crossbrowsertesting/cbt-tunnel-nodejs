75e4dc1d007c291fe287b556fecc90c14fe0c3fd
Warner Johnson <wwjhnsn2@memphis.edu>
2015-12-21 13:27:23 -0600

    commit fix

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ var _ = require('lodash'),
     gfx = require('./gfx.js'),
     warn = gfx.warn,
     help = gfx.help,
-    version = '0.0.19';
+    version = '0.0.2';
 
 module.exports = {
     checkVersion: function(data,params){
