8e1c7f4fae187adc62515b773605715f417a6aca
Warner Johnson <wwjhnsn2@memphis.edu>
2015-12-22 16:43:25 -0600

    version update

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ var _ = require('lodash'),
     gfx = require('./gfx.js'),
     warn = gfx.warn,
     help = gfx.help,
-    version = '0.0.2';
+    version = '0.0.21';
 
 module.exports = {
     checkVersion: function(data,params){
