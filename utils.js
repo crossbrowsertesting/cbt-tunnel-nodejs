a377a3408082c230ba06e7583a96d5685b2484c1
bosh <bosh@Hati.local>
2016-10-13 15:21:51 -0500

    updated version number

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ var _ = require('lodash'),
     gfx = require('./gfx.js'),
     warn = gfx.warn,
     help = gfx.help,
-    version = '0.0.27';
+    version = '0.0.28';
 
 module.exports = {
     checkVersion: function(data,params){
