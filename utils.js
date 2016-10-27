236237e40c985d7cbb794f4e84b05928f4b6213d
cbtadmin <cbtadmin@VM-Server-1.local>
2016-10-27 11:44:14 -0500

    updated version number

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ var _ = require('lodash'),
     gfx = require('./gfx.js'),
     warn = gfx.warn,
     help = gfx.help,
-    version = '0.0.28';
+    version = '0.0.29';
 
 module.exports = {
     checkVersion: function(data,params){
