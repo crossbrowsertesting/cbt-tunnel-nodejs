fc524af416c587fcc66edbe8d973a48445dac699
warner <warner@crossbrowsertesting.com>
2017-04-17 16:43:51 -0500

    version update -- allows for env variables in lieu of flags

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ var _ = require('lodash'),
     gfx = require('./gfx.js'),
     warn = gfx.warn,
     help = gfx.help,
-    version = '0.0.34';
+    version = '0.0.35';
 
 module.exports = {
     checkVersion: function(data,params){
