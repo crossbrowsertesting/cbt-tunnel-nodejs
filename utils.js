9f76abad027c84545a9163196c71b87d24f527f4
warner <warner@crossbrowsertesting.com>
2017-06-05 16:45:17 -0500

    version bump

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ var _ = require('lodash'),
     gfx = require('./gfx.js'),
     warn = gfx.warn,
     help = gfx.help,
-    version = '0.0.35';
+    version = '0.1.0';
 
 module.exports = {
     checkVersion: function(data,params){
